2691a94a0963201f7f27cfddb6043c753f3c0887
justinliu2 <188193223@qq.com>
2014-07-31 19:33:20 +0800

    more EX

--- HTDP_EX_37.rkt
+++ HTDP_EX_37.rkt
@@ -1,6 +1,6 @@
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
-#reader(lib "htdp-advanced-reader.ss" "lang")((modname HTDP_SAMPLE-PROBLEM_3.6) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #t #t none #f ())))
+#reader(lib "htdp-advanced-reader.ss" "lang")((modname HTDP_EX_37) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #t #t none #f ())))
 (require 2htdp/image)
 (require 2htdp/universe)
 (define WHEEL-RADIUS 5)
@@ -20,12 +20,22 @@
     "red")
    BOTH-WHEELS))
 (define Y-CAR (image-height CAR))
-(define BACKGROUND (empty-scene 400 30))
+(define tree
+  (underlay/xy (circle 10 'solid 'green )
+               9 15
+               (rectangle 2 20 'solid 'brown)))
+(define BACKGROUND
+  (place-image tree
+               20 20 
+               (empty-scene 400 30)))
 (define (render ws)
   (place-image CAR ws Y-CAR BACKGROUND))
 (define (tock ws)
   (+ ws 3))
+(define (end? ws)
+  (> ws 400))
 (define (main ws)
   (big-bang ws
             [on-tick tock]
-            [to-draw render]))
+            [to-draw render]
+            [stop-when end?]))
